78ce27f9f3069ecf9917662de9dc5df00512e2aa
JustKaneri <justkaneri@yandex.ru>
2023-10-20 20:25:28 +0300

    authServis create auth controller

---
+++ Price_Hunt/Auth_Servise/Controllers/v1/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Auth_Servise.Dto;
+using Auth_Servise.IntefaceRepository;
+using Auth_Servise.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth_Servise.Controllers.v1
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class AuthController : Controller
+    {
+        private readonly IRegestryRepository _regestryRepository;
+
+        public AuthController(IRegestryRepository regestryRepository)
+        {
+            _regestryRepository = regestryRepository;
+        }
+
+
+        [HttpPost("authorization")]
+        public async Task<IActionResult> Auth(UserRegestryDto user)
+        {
+            Token token = await _regestryRepository.Regestry(user);
+
+            return Ok(token);
+        }
+    }
+}
